cf99235a461253597985b7332107675f7aa4309b
Petr Knap <dev@petrknap.cz>
2016-03-05 17:30:03 +0100

    Added FactoryInterface

---
+++ src/ServiceManager/FactoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PetrKnap\Php\ServiceManager;
+
+use Zend\ServiceManager\Factory\FactoryInterface as RealFactoryInterface;
+
+interface FactoryInterface extends RealFactoryInterface
+{
+    // See RealFactoryInterface
+}
