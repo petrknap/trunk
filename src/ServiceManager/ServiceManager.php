46f51b2785dd31a68cab29e0c5409458a452fa7b
Petr Knap <dev@petrknap.cz>
2016-03-05 12:36:47 +0100

    Service manager

---
+++ src/ServiceManager/ServiceManager.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace PetrKnap\Php\ServiceManager;
+
+use PetrKnap\Php\Singleton\SingletonInterface;
+use PetrKnap\Php\Singleton\SingletonTrait;
+use Zend\ServiceManager\ServiceManager as RealServiceManager;
+
+/**
+ * Service manager
+ *
+ * @author   Petr Knap <dev@petrknap.cz>
+ * @since    2016-03-05
+ * @category Patterns
+ * @package  PetrKnap\Php\ServiceManager
+ * @version  0.1
+ * @license  https://github.com/petrknap/php-servicemanager/blob/master/LICENSE MIT
+ */
+class ServiceManager implements ServiceLocatorInterface, SingletonInterface
+{
+    use SingletonTrait;
+
+    /**
+     * @var array
+     */
+    private static $config = [];
+
+    /**
+     * Sets (overrides) configuration
+     *
+     * @param array $config
+     */
+    public static function setConfig(array $config)
+    {
+        // TODO add has instance check - exception
+        // TODO add non-empty config check - trigger_error(..., E_USER_NOTICE);
+        self::$config = $config;
+    }
+
+    /**
+     * Sets (appends) configuration
+     *
+     * @param array $config
+     */
+    public static function addConfig(array $config)
+    {
+        // TODO add has instance check - exception
+        // TODO add override check - trigger_error(..., E_USER_WARNING);
+        self::$config = array_replace_recursive(self::$config, $config);
+    }
+
+    /**
+     * Returns current config
+     *
+     * @return array
+     */
+    public static function getConfig()
+    {
+        return self::$config;
+    }
+
+    /**
+     * @var RealServiceManager
+     */
+    private $serviceManager;
+
+    protected function __construct()
+    {
+        $this->serviceManager = new RealServiceManager(self::$config);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function get($name)
+    {
+        return $this->serviceManager->get($name);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function has($name)
+    {
+        return $this->serviceManager->has($name);
+    }
+}
---
+++ tests/ServiceManager/ServiceManagerTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace PetrKnap\Test\Php\ServiceManager;
+
+use PetrKnap\Php\ServiceManager\ConfigBuilder;
+use PetrKnap\Php\ServiceManager\ServiceManager;
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+class ServiceManagerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreatesValidInstance()
+    {
+        $instance = ServiceManager::getInstance();
+
+        $this->assertInstanceOf("PetrKnap\\Php\\ServiceManager\\ServiceLocatorInterface", $instance);
+        $this->assertInstanceOf("PetrKnap\\Php\\ServiceManager\\ServiceManager", $instance);
+    }
+
+    public function setAddGetConfigWorksDataProvider()
+    {
+        return [
+            [[], ["A" => ["B" => "C"]], true, ["A" => ["B" => "C"]]],
+            [[], ["A" => ["B" => "C"]], false, ["A" => ["B" => "C"]]],
+            [["A" => ["B" => "C"]], ["A" => ["B" => "D"]], true, ["A" => ["B" => "D"]]],
+            [["A" => ["B" => "C"]], ["A" => ["B" => "D"]], false, ["A" => ["B" => "D"]]],
+            [["A" => ["B" => "C"]], ["A" => ["D" => "E"]], true, ["A" => ["D" => "E"]]],
+            [["A" => ["B" => "C"]], ["A" => ["D" => "E"]], false, ["A" => ["B" => "C", "D" => "E"]]]
+        ];
+    }
+
+    /**
+     * @dataProvider setAddGetConfigWorksDataProvider
+     *
+     * @param array $initialConfig
+     * @param array $config
+     * @param bool $override
+     * @param array $expectedConfig
+     */
+    public function testSetAddGetConfigWorks(array $initialConfig, array $config, $override, array $expectedConfig)
+    {
+        ServiceManager::setConfig($initialConfig);
+        if ($override) {
+            ServiceManager::setConfig($config);
+        } else {
+            ServiceManager::addConfig($config);
+        }
+
+        $this->assertEquals($expectedConfig, ServiceManager::getConfig());
+    }
+
+    public function testGetWorks()
+    {
+        ServiceManager::setConfig([ConfigBuilder::SERVICES => ["this" => $this]]);
+
+        $this->assertInstanceOf(get_class($this), ServiceManager::getInstance()->get("this"));
+    }
+
+    public function testHasWorks()
+    {
+        ServiceManager::setConfig([ConfigBuilder::SERVICES => ["this" => $this]]);
+
+        $this->assertTrue(ServiceManager::getInstance()->has("this"));
+    }
+}
