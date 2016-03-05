d76c5e2c2ae719021641a39c58ab50a782ffeeb0
Petr Knap <dev@petrknap.cz>
2016-03-05 12:35:49 +0100

    Service locator interface

---
+++ src/ServiceManager/ServiceLocatorInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PetrKnap\Php\ServiceManager;
+
+use Interop\Container\ContainerInterface;
+
+interface ServiceLocatorInterface extends ContainerInterface
+{
+    // See ContainerInterface
+}
