ff44ef4f8cd3366e4a46755e61101a8b91a52bb9
Petr Knap <dev@petrknap.cz>
2016-03-06 10:50:25 +0100

    Reformatted

--- tests/ServiceManager/ServiceManagerTest/DependentServiceFactory.php
+++ tests/ServiceManager/ServiceManagerTest/DependentServiceFactory.php
@@ -3,10 +3,8 @@
 namespace PetrKnap\Test\Php\ServiceManager\ServiceManagerTest;
 
 use PetrKnap\Php\ServiceManager\ConfigBuilder;
-use PetrKnap\Php\ServiceManager\Exception\ServiceLocatorException;
 use PetrKnap\Php\ServiceManager\FactoryInterface;
 use PetrKnap\Php\ServiceManager\ServiceLocatorInterface;
-use Zend\ServiceManager\Exception\ServiceNotCreatedException;
 
 class DependentServiceFactory implements FactoryInterface
 {
@@ -19,14 +17,6 @@ class DependentServiceFactory implements FactoryInterface
         return $config->getConfig();
     }
 
-    /**
-     * Create an object
-     *
-     * @param  ServiceLocatorInterface $serviceLocator
-     * @throws ServiceNotCreatedException error while creating the service
-     * @throws ServiceLocatorException if any other error occurs
-     * @return object
-     */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
         /** @var IndependentService $independentService */
