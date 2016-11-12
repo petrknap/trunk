3a2dd303eef427c4aa16a1d9d6590bd347fddbd4
Petr Knap <dev@petrknap.cz>
2016-11-12 11:31:10 +0100

    Added methods getBootstrapClass and clearTemp

--- src/Bootstrap/PhpUnit/NetteTestCase.php
+++ src/Bootstrap/PhpUnit/NetteTestCase.php
@@ -18,21 +18,27 @@ abstract class NetteTestCase extends \PHPUnit_Framework_TestCase
     private static $container;
 
     /**
+     * @return string
+     */
+    protected static function getBootstrapClass()
+    {
+        if (static::NETTE_BOOTSTRAP_CLASS !== null) {
+            return static::NETTE_BOOTSTRAP_CLASS;
+        } elseif (defined("NETTE_BOOTSTRAP_CLASS")) {
+            return NETTE_BOOTSTRAP_CLASS;
+        } else {
+            throw new \RuntimeException("Unknown Nette Bootstrap class");
+        }
+    }
+
+    /**
      * @inheritdoc
      */
     public static function setUpBeforeClass()
     {
         parent::setUpBeforeClass();
 
-        if (static::NETTE_BOOTSTRAP_CLASS !== null) {
-            $bootstrapClass = static::NETTE_BOOTSTRAP_CLASS;
-        } elseif (defined("NETTE_BOOTSTRAP_CLASS")) {
-            $bootstrapClass = NETTE_BOOTSTRAP_CLASS;
-        } else {
-            throw new \RuntimeException("Unknown Nette Bootstrap class");
-        }
-
-        self::$container = call_user_func("{$bootstrapClass}::getContainer", array(
+        self::$container = call_user_func(self::getBootstrapClass() . "::getContainer", array(
             Bootstrap::OPTION_IS_TEST_RUN => true
         ));
     }
@@ -43,6 +49,31 @@ abstract class NetteTestCase extends \PHPUnit_Framework_TestCase
     protected function getContainer()
     {
         return self::$container;
+    }
+
+    /**
+     * @return void
+     */
+    protected function clearTemp()
+    {
+        /** @var Bootstrap $bootstrap */
+        $bootstrapClass = self::getBootstrapClass();
+        $bootstrap = @new $bootstrapClass;
+
+        $bootstrapReflection = new \ReflectionClass($bootstrapClass);
+        $getTempDir = $bootstrapReflection->getMethod("getTempDir");
+        $getTempDir->setAccessible(true);
+        $tempDir = $getTempDir->invoke($bootstrap);
+
+        $rdi = new \RecursiveDirectoryIterator($tempDir, \RecursiveDirectoryIterator::SKIP_DOTS);
+        $rii = new \RecursiveIteratorIterator($rdi, \RecursiveIteratorIterator::CHILD_FIRST);
+        foreach($rii as $item) {
+            if ($item->isDir()){
+                rmdir($item->getRealPath());
+            } else {
+                unlink($item->getRealPath());
+            }
+        }
     }
 
     /**
