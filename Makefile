68e75c6aa7ca28a873a7ce4ac81cb2bed5c66cf9
Petr Knap <dev@petrknap.cz>
2017-03-12 10:17:48 +0100

    Reorganised file structure

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ docker:
 	sudo docker build -t petrknap/php .
 
 docker-run:
-	sudo docker run -v $$(pwd):/app --rm petrknap/php bash -c 'cd /app && ${ARGS}'
+	sudo docker run -v $$(pwd):/app --rm petrknap/php bash -c "cd /app && ${ARGS}"
 
 composer:
 	make docker-run ARGS="composer ${ARGS}"
@@ -20,14 +20,5 @@ composer-update:
 synchronization:
 	make docker-run ARGS="./bin/synchronize.php"
 
-tests:
-	rsync -r ./packages/ ./temp/packages/
-	make .forPackages ARGS=".testPackage"
-
-.forPackages:
-	for package in enum; do \
-		make ${ARGS} ARGS="$$package"; \
-	done;
-
-.testPackage:
-	make docker-run ARGS="cd ./temp/packages/${ARGS} && composer install && ./vendor/bin/phpunit"
+tests: composer-install
+	make docker-run ARGS="./vendor/bin/phpunit ${ARGS}"
--- bin/synchronize.php
+++ bin/synchronize.php
@@ -3,7 +3,7 @@
 
 $synchronize = new Synchronize();
 
-foreach(scandir(__DIR__ . "/../packages/") as $package) {
+foreach(scandir(__DIR__ . "/../src/") as $package) {
     if (in_array($package, array(".", ".."))) {
         continue;
     }
@@ -14,10 +14,14 @@ foreach(scandir(__DIR__ . "/../packages/") as $package) {
     print(" [done]\n");
 
     print("\t* Update LICENSE");
-    copy(__DIR__ . "/../LICENSE", __DIR__ . "/../packages/" . $package . "/LICENSE");
+    $synchronize->license($package);
     print(" [done]\n");
 
     print("\t* Update composer.json");
+    $synchronize->composer($package);
+    print(" [done]\n");
+
+    print("\t* Update phpunit.xml");
     $synchronize->composer($package);
     print(" [done]\n");
 }
@@ -36,23 +40,34 @@ class Synchronize
         ];
     }
 
-    function composer($package)
+    public function license($package)
     {
-        $composerFile = __DIR__ . "/../packages/" . $package . "/composer.json";
+        copy(__DIR__ . "/../LICENSE", __DIR__ . "/../src/" . $package . "/LICENSE");
+    }
+
+    public function composer($package)
+    {
+        $composerFile = __DIR__ . "/../src/" . $package . "/composer.json";
         $composer = json_decode(file_get_contents($composerFile), true);
 
-        $composer["name"] = $this->composer["name"] . "-" . $package;
-        $composer["homepage"] = $this->composer["homepage"] . "-" . $package;
+        $composer["name"] = $this->composer["name"] . "-" . strtolower($package);
+        $composer["homepage"] = $this->composer["homepage"] . "-" . strtolower($package);
         $composer["license"] = $this->composer["license"];
         $composer["authors"] = $this->composer["authors"];
         $composer["require"] = array_merge($composer["require"], $this->composer["require"]);
+        $composer["autoload"] = array("psr-4" => array("PetrKnap\\Php\\" . $package ."\\" => "."));
 
         file_put_contents($composerFile, json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . PHP_EOL);
     }
 
-    function registerPackage($package)
+    public function phpunit($package)
     {
-        $this->composer["require-dev"]["petrknap/php-" . $package] = "dev-master";
+        copy(__DIR__ . "/../phpunit.xml", __DIR__ . "/../src/" . $package . "/phpunit.xml");
+    }
+
+    public function registerPackage($package)
+    {
+        $this->composer["require-dev"]["petrknap/php-" . strtolower($package)] = "dev-master";
         file_put_contents($this->composerFile, json_encode($this->composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . PHP_EOL);
     }
 }
