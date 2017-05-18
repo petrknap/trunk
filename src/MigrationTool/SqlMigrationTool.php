74f3d7fdd46797add29859a8fe7c4b8e82ef88e6
Filip Halaxa <filip@halaxa.cz>
2017-05-18 13:56:35 +0200

    Update SqlMigrationTool.php
    Unified table existence check

--- src/MigrationTool/SqlMigrationTool.php
+++ src/MigrationTool/SqlMigrationTool.php
@@ -38,8 +38,21 @@ abstract class SqlMigrationTool extends AbstractMigrationTool
      */
     protected function createMigrationTable()
     {
-        /** @noinspection SqlNoDataSourceInspection,SqlDialectInspection */
-        if (false === $this->getPhpDataObject()->exec("SELECT null FROM {$this->getNameOfMigrationTable()} LIMIT 1")) {
+        $errmode = $this->getPhpDataObject()->getAttribute(\PDO::ATTR_ERRMODE);
+        $this->getPhpDataObject()->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        try {
+            /** @noinspection SqlNoDataSourceInspection,SqlDialectInspection */
+            $result = $this->getPhpDataObject()->query("SELECT null FROM {$this->getNameOfMigrationTable()} LIMIT 1")->fetchAll();
+        } catch (\PDOException $e) {
+            if ($e->getCode() === '42S02') {
+               $result = false;
+            } else {
+                throw $e;
+            }
+        }
+        $this->getPhpDataObject()->setAttribute(\PDO::ATTR_ERRMODE, $errmode);
+
+        if (false === $result) {
             /** @noinspection SqlNoDataSourceInspection,SqlDialectInspection */
             $result = $this->getPhpDataObject()->exec(
                 "CREATE TABLE IF NOT EXISTS {$this->getNameOfMigrationTable()}" .
