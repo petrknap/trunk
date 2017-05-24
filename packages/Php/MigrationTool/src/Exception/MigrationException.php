7d8268eea6ee772989882015bd322bccadf92977
Petr Knap <dev@petrknap.cz>
2017-05-24 09:22:14 +0200

    Hot-fixed "A non well formed numeric value encountered"

--- packages/Php/MigrationTool/src/Exception/MigrationException.php
+++ packages/Php/MigrationTool/src/Exception/MigrationException.php
@@ -4,5 +4,14 @@ namespace PetrKnap\Php\MigrationTool\Exception;
 
 abstract class MigrationException extends \Exception
 {
-    // Empty class
+    /**
+     * @inheritdoc
+     * @param string $code
+     */
+    public function __construct($message = "", $code = 0, $previous = null)
+    {
+        // Hot-fixed "A non well formed numeric value encountered"
+        parent::__construct($message, 0, $previous);
+        $this->code = $code;
+    }
 }
