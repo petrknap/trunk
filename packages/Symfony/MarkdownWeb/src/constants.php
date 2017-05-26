70f6e56215527ce05333f09b33a6b7422c560499
Petr Knap <dev@petrknap.cz>
2017-05-26 21:34:43 +0200

    Added forgotten constant

--- packages/Symfony/MarkdownWeb/src/constants.php
+++ packages/Symfony/MarkdownWeb/src/constants.php
@@ -4,5 +4,6 @@ namespace PetrKnap\Symfony\MarkdownWeb;
 
 const BUNDLE_NAME = "MarkdownWeb";
 const BUNDLE_ALIAS = "markdown_web";
+const BUNDLE_CONSOLE = "markdown-web";
 const TWIG_EXTENSION = BUNDLE_ALIAS . ".twig";
 const CRAWLER_SERVICE = BUNDLE_ALIAS . ".crawler";
