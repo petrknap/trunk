aa3d5d546616ae98e6c94d860a51a703e186c997
Petr Knap <dev@petrknap.cz>
2017-11-05 10:23:14 +0100

    Improved test

--- projects/storyboard/tests/LogFileProcessorTest.php
+++ projects/storyboard/tests/LogFileProcessorTest.php
@@ -6,11 +6,17 @@ use Storyboard\LogFileProcessor;
 
 class LogFileProcessorTest extends TestCase
 {
-    public function testCanProcessFile()
+    public function testWorksWhenFileIsValid()
     {
         $this->assertSame(
-            file_get_contents(__DIR__ . '/data/file.log.expect'),
-            (new LogFileProcessor())->processFile(__DIR__ . '/data/file.log')
+            file_get_contents(__DIR__ . '/data/valid.log.expect'),
+            (new LogFileProcessor())->processFile(__DIR__ . '/data/valid.log')
         );
+    }
+
+    public function testThrowsWhenFileIsInvalid()
+    {
+        $this->expectException(\RuntimeException::class);
+        (new LogFileProcessor())->processFile(__DIR__ . '/data/invalid.log');
     }
 }
