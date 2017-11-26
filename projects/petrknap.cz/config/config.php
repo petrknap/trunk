e0fe2d2b862ec9a167ddf4dae28e556e22d36c6e
Petr Knap <dev@petrknap.cz>
2017-11-26 22:51:03 +0100

    Added GA

--- projects/petrknap.cz/config/config.php
+++ projects/petrknap.cz/config/config.php
@@ -8,12 +8,14 @@ define('CONFIG_DB_MIGRATIONS_DIR', 'db_migrations_dir');
 define('CONFIG_CACHE_DIR', 'cache_dir');
 define('CONFIG_CACHE_REMOTE_CONTENT_NAMESPACE', 'cache_remote_content_namespace');
 define('CONFIG_CACHE_REMOTE_CONTENT_LIFETIME', 'cache_remote_content_lifetime');
+define('CONFIG_GA_TRACKING_ID', 'ga_tracking_id');
 
 ${CONFIG} = [
     CONFIG_DB_MIGRATIONS_DIR => __DIR__ . '/../migrations',
     CONFIG_CACHE_DIR => __DIR__ . '/../var/cache',
     CONFIG_CACHE_REMOTE_CONTENT_NAMESPACE => 'remote_content',
     CONFIG_CACHE_REMOTE_CONTENT_LIFETIME => 7 * 24 * 3600,
+    CONFIG_GA_TRACKING_ID => 'UA-88031264-1',
 ];
 
 require_once __DIR__ . '/config.local.php';
--- projects/petrknap.cz/config/services.php
+++ projects/petrknap.cz/config/services.php
@@ -10,11 +10,17 @@ use PetrKnap\Php\MigrationTool\SqlMigrationTool;
 use PetrKnap\Php\ServiceManager\ConfigurationBuilder;
 use PetrKnap\Php\ServiceManager\ServiceManager;
 use Psr\Container\ContainerInterface;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use TheIconic\Tracking\GoogleAnalytics\Analytics;
 
 $cb = new ConfigurationBuilder();
 $cb->setSharedByDefault(true);
 
 $cb->addService(CONFIG, ${CONFIG});
+$cb->addService(Request::class, Request::createFromGlobals());
 $cb->addFactory(\PDO::class, function (ContainerInterface $container) {
     $config = $container->get(CONFIG);
 
@@ -34,6 +40,42 @@ $cb->addFactory(SqlMigrationTool::class, function (ContainerInterface $container
         $container->get(\PDO::class)
     );
 });
+$cb->addFactory(Analytics::class, function (ContainerInterface $container) {
+    $config = $container->get(CONFIG);
+    /** @var Request $request */
+    $request = $container->get(Request::class);
+
+    if ($request->cookies->has('ga_client_id')) {
+        $clientId = $request->cookies->get('ga_client_id');
+    } else {
+        $clientId = Uuid::uuid4()->toString();
+        $response = new Response();
+        $response->headers->setCookie(new Cookie('ga_client_id', $clientId, new DateTime('+2 years')));
+        $response->sendHeaders();
+    }
+
+    $analytics = (new Analytics(true))
+        ->setProtocolVersion('1') // https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
+        ->setTrackingId($config[CONFIG_GA_TRACKING_ID])
+        ->setDataSource('web')
+        ->setClientId($clientId)
+        ->setIpOverride($request->getClientIp())
+        ->setUserLanguage($request->getPreferredLanguage())
+        ->setHitType('pageview')
+        ->setDocumentHostName($request->getHost())
+        ->setDocumentPath($request->getRequestUri());
+
+    if ($request->headers->has('User-Agent')) {
+        $analytics->setUserAgentOverride($request->headers->get('User-Agent'));
+    }
+
+    if ($request->headers->has('Referer')) {
+        $analytics->setUserAgentOverride($request->headers->get('Referer'));
+    }
+
+    return $analytics;
+});
+$cb->setShared(Analytics::class, false);
 $cb->addFactory(RemoteContentAccessor::class, RemoteContentAccessorFactory::class);
 $cb->addFactory(RemoteContentCache::class, RemoteContentCacheFactory::class);
 $cb->addFactory(UrlShortenerService::class, UrlShortenerServiceFactory::class);
