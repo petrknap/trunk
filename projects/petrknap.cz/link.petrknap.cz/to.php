1571dac6b15c7bec798c96b11554d3df1d96c2f8
Petr Knap <dev@petrknap.cz>
2017-11-28 22:59:14 +0100

    Improved to.php

--- projects/petrknap.cz/link.petrknap.cz/to.php
+++ projects/petrknap.cz/link.petrknap.cz/to.php
@@ -2,15 +2,20 @@
 
 use PetrKnap\Php\ServiceManager\ServiceManager;
 use PetrKnapCz\UrlShortener\UrlShortenerService;
+use Symfony\Component\HttpFoundation\Response;
 use TheIconic\Tracking\GoogleAnalytics\Analytics;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-ServiceManager::getInstance()
-    ->get(Analytics::class)
-    ->sendPageView();
+if (isset($_GET['keyword']) && is_string($_GET['keyword'])) {
+    ServiceManager::getInstance()
+        ->get(Analytics::class)
+        ->sendPageView();
 
-ServiceManager::getInstance()
-    ->get(UrlShortenerService::class)
-    ->getResponse($_GET['keyword'])
-    ->send();
+    ServiceManager::getInstance()
+        ->get(UrlShortenerService::class)
+        ->getResponse($_GET['keyword'])
+        ->send();
+} else {
+    (new Response(null, Response::HTTP_FORBIDDEN))->send();
+}
