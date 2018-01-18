ef6c7c30a1d8f3b56278dbb034d42f7386229821
Petr Knap <dev@petrknap.cz>
2018-01-18 20:15:46 +0100

    Finalized table

--- projects/petrknap.cz/migrations/2017-11-28.1 - url_shortener__records.sql
+++ projects/petrknap.cz/migrations/2017-11-28.1 - url_shortener__records.sql
@@ -2,5 +2,7 @@ CREATE TABLE url_shortener__records (
   id int(11) NOT NULL,
   keyword varchar(64) NOT NULL,
   url varchar(2048) NOT NULL,
-  is_redirect tinyint(1) NOT NULL
+  is_redirect tinyint(1) NOT NULL,
+  UNIQUE INDEX unique_keyword (keyword),
+  PRIMARY KEY(id)
 );
