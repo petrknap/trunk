673aa244d58d7e154ab70dbcf53655f3cb8be8c2
Petr Knap <dev@petrknap.cz>
2018-01-18 20:16:45 +0100

    Added migrate command

---
+++ projects/petrknap.cz/api.petrknap.cz/migrate.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PetrKnapCz;
+
+use PetrKnap\Php\MigrationTool\SqlMigrationTool;
+use Symfony\Component\HttpFoundation\Request;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+/** @noinspection PhpParamsInspection */
+authorize(container()->get(Request::class));
+
+container()->get(SqlMigrationTool::class)
+    ->migrate();
