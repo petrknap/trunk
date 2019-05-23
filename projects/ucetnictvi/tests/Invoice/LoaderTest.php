f460ecc5ef4129ef0526f00e82eea4a98bb76c36
Petr Knap <dev@petrknap.cz>
2019-05-23 22:06:03 +0200

    Added first failing test

---
+++ projects/ucetnictvi/tests/Invoice/LoaderTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ucetnictvi\Test\Invoice;
+
+use PHPUnit\Framework\TestCase;
+
+class LoaderTest extends TestCase
+{
+    public function testFail()
+    {
+        $this->fail();
+    }
+}
