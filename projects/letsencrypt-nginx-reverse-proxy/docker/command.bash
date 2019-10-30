6c9a353f9ce4aef87ab9668f32a36999233c0d66
Petr Knap <dev@petrknap.cz>
2019-10-30 10:20:20 +0100

    Kill replaced by reload

--- projects/letsencrypt-nginx-reverse-proxy/docker/command.bash
+++ projects/letsencrypt-nginx-reverse-proxy/docker/command.bash
@@ -3,19 +3,14 @@ set -e
 
 bash /prepare_nginx.bash
 nginx -g "daemon off;" &
-NGINX_PID=$!
 bash /call_letsencrypt.bash
-kill -2 "${NGINX_PID}"
-sleep 2
+nginx -s reload
 
 while (true); do (
-    nginx -g "daemon off;" &
-    NGINX_PID=$!
     for I in $(seq 1 100800); do (
         sleep 5
         curl --max-time 2 -f http://127.0.0.1/ > /dev/null 2>&1
     ); done
     bash /call_letsencrypt.bash
-    kill -2 "${NGINX_PID}"
-    sleep 2
+    nginx -s reload
 ); done
