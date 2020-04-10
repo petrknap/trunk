89c378d6417efb60958989641530f33cda34caaf
Petr Knap <dev@petrknap.cz>
2020-04-10 12:27:14 +0200

    Removed reveal support

--- projects/petrknap.cz/petrknap.cz/index.php
+++ projects/petrknap.cz/petrknap.cz/index.php
@@ -7,15 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$request = Request::createFromGlobals();
-
-$response = container()
+container()
     ->get(RemoteContentAccessor::class)
-    ->getResponse('https://petrknap.github.io/index_cz.html');
-
-$response = container()
-    ->get(RavealAsPages::class)
-    ->modifyResponse($request, $response);
-
-/** @var Response $response */
-$response->send();
+    ->getResponse('https://petrknap.github.io/index_cz.html')
+    ->send();
