be88d274a51482ba56f2cac2818b73e0148ff2cd
Petr Knap <dev@petrknap.cz>
2020-04-22 14:33:41 +0200

    Added support for sitemap.xml, robots.txt and 404 page

--- projects/petrknap.cz/petrknap.cz/index.php
+++ projects/petrknap.cz/petrknap.cz/index.php
@@ -2,12 +2,28 @@
 
 namespace PetrKnapCz;
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$uri = $_GET['uri'] ?? '';
+switch ($uri) {
+    case '':
+        $uri = 'index_cz.html';
+        break;
+    case 'sitemap.xml':
+        header("Content-Type: text/xml");
+        break;
+    case 'robots.txt':
+        header("Content-Type: text/plain");
+        break;
+    default:
+        http_response_code(Response::HTTP_NOT_FOUND);
+        header("Content-Type: text/plain");
+        die('Page Not Found');
+}
+
 container()
     ->get(RemoteContentAccessor::class)
-    ->getResponse('https://petrknap.github.io/index_cz.html')
+    ->getResponse("https://petrknap.github.io/{$uri}")
     ->send();
