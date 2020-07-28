36f0ef3bf58dc0c88d5cbaf37374f2927133c8d4
Petr Knap <dev@petrknap.cz>
2020-07-28 18:32:36 +0200

    Simple Patches object

---
+++ packages/Doctrine/UnalterableMigrations/src/Patches.php
@@ -0,0 +1,79 @@
+<?php declare(strict_types=1);
+
+namespace PetrKnap\Doctrine\UnalterableMigrations;
+
+class Patches
+{
+    const BEFORE_FIRST_LINE = 0;
+
+    private $payload;
+
+    private function __construct(string $payload)
+    {
+        $this->payload = $payload;
+    }
+
+    public static function on(string $payload): self
+    {
+        return new self($payload);
+    }
+
+    public function insertLine(int $number, string $content): self
+    {
+        $lines = explode(PHP_EOL, $this->payload);
+
+        if ($number === self::BEFORE_FIRST_LINE) {
+            $this->payload = $content . PHP_EOL . $this->payload;
+        } elseif($number === count($lines)) {
+            $this->payload = $this->payload . PHP_EOL . $content;
+        } else {
+            $this->checkLineNumber($lines, $number);
+
+            array_splice($lines, $number, 0, [$content]);
+
+            $this->payload = implode(PHP_EOL, $lines);
+        }
+
+        return $this;
+    }
+
+    public function removeLine(int $number, string $contains = null): self
+    {
+        $lines = explode(PHP_EOL, $this->payload);
+
+        $this->checkLineNumber($lines, $number);
+        if ($contains) {
+            $this->checkLineContent($lines[$number], $contains);
+        }
+
+        unset($lines[$number]);
+
+        $this->payload = implode(PHP_EOL, $lines);
+
+        return $this;
+    }
+
+    public function apply(): string
+    {
+        return $this->payload;
+    }
+
+    public function __toString()
+    {
+        return $this->apply();
+    }
+
+    private function checkLineNumber(array $lines, int $number): void
+    {
+        if ($number < 0 || $number >= count($lines)) {
+            throw new \LogicException("Line {$number} does not exist");
+        }
+    }
+
+    private function checkLineContent(string $content, string $contains): void
+    {
+        if (strpos($content, $contains) === false) {
+            throw new \LogicException("Line \"{$content}\" does not contain \"{$contains}\"");
+        }
+    }
+}
---
+++ packages/Doctrine/UnalterableMigrations/tests/PatchesTest.php
@@ -0,0 +1,78 @@
+<?php declare(strict_types=1);
+
+namespace PetrKnap\Doctrine\UnalterableMigrations\Tests;
+
+use PetrKnap\Doctrine\UnalterableMigrations\Patches;
+use PHPUnit\Framework\TestCase;
+
+class PatchesTest extends TestCase
+{
+    const PAYLOAD = "a\nb\nc";
+
+    /**
+     * @dataProvider dataRemoveLineRemovesCorrectLine
+     * @noinspection PhpDocSignatureInspection
+     */
+    public function testRemoveLineRemovesCorrectLine(int $lineNumber, ?string $contains, string $expected): void
+    {
+        $this->assertSame(
+            $expected,
+            Patches::on(self::PAYLOAD)->removeLine($lineNumber, $contains)->apply()
+        );
+    }
+
+    public function dataRemoveLineRemovesCorrectLine(): array
+    {
+        return [
+            [0, null, "b\nc"],
+            [0, 'a', "b\nc"],
+            [1, null, "a\nc"],
+            [1, 'b', "a\nc"],
+            [2, null, "a\nb"],
+            [2, 'c', "a\nb"],
+        ];
+    }
+
+    public function testRemoveLineThrowsWhenLineIsOutOfRange(): void
+    {
+        $this->expectException(\LogicException::class);
+
+        Patches::on(self::PAYLOAD)->removeLine(3);
+    }
+
+    public function testRemoveLineThrowsWhenLineDoesNotContainExpectedString(): void
+    {
+        $this->expectException(\LogicException::class);
+
+        Patches::on(self::PAYLOAD)->removeLine(1, 'c');
+    }
+
+    /**
+     * @dataProvider dataInsertLineInsertsCorrectLine
+     * @noinspection PhpDocSignatureInspection
+     */
+    public function testInsertLineInsertsCorrectLine(int $lineNumber, string $expected): void
+    {
+        $this->assertSame(
+            $expected,
+            Patches::on(self::PAYLOAD)->insertLine($lineNumber, 'x')->apply()
+        );
+    }
+
+    public function dataInsertLineInsertsCorrectLine(): array
+    {
+        return [
+            [Patches::BEFORE_FIRST_LINE, "x\na\nb\nc"],
+            [1, "a\nx\nb\nc"],
+            [2, "a\nb\nx\nc"],
+            [3, "a\nb\nc\nx"],
+        ];
+    }
+
+    public function testInsertLineThrowsWhenLineIsOutOfRange(): void
+    {
+        $this->expectException(\LogicException::class);
+
+        Patches::on(self::PAYLOAD)->removeLine(3);
+    }
+}
