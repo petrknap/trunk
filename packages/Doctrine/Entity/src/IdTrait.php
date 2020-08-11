23e0fd3dc82af3b9ed5cd55304416899629e6b67
Petr Knap <dev@petrknap.cz>
2020-08-11 12:07:11 +0200

    Entity helpers for Doctrine

---
+++ packages/Doctrine/Entity/src/IdTrait.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace PetrKnap\Doctrine\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait IdTrait
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): int
+    {
+        if ($this->id === null) {
+            throw new \RuntimeException(get_class($this) . ' has not been persisted');
+        }
+
+        return $this->id;
+    }
+}
---
+++ projects/petrknap.github.io/docs/doctrine-entity.md
@@ -0,0 +1,7 @@
+---
+layout: blueprint
+---
+# Entity helpers for Doctrine
+
+
+{% include docs/how-to-install.md %}
