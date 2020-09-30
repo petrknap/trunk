c0bbeca386684ef2033bdc571ad092ab0061ba94
Petr Knap <dev@petrknap.cz>
2020-09-30 12:34:19 +0200

    Added support for work with localhost

--- docker/selenium-needle/README.md
+++ docker/selenium-needle/README.md
@@ -5,6 +5,7 @@ See [`example.py`](example.py) to know how to write basic test
 and [`bin/nosetests.sh`](bin/nosetests.sh) to know how to run it.
 
 For first time run `bin/nosetests.sh example.py --with-save-baseline` and create baseline.
-Another times run `bin/nosetests.sh example.py` to compare current state against baseline.
+Another time run `bin/nosetests.sh example.py` to compare current state against baseline.
+Use `host.local` to access your localhost or take advantage of your hosts file.
 
 For more details visit [Needle](https://needle.readthedocs.io/), [Selenium](https://selenium-python.readthedocs.io/) and [Nose](https://nose.readthedocs.io/) documentations.
--- docker/selenium-needle/bin/nosetests.sh
+++ docker/selenium-needle/bin/nosetests.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 set -e
-set -x
+# set -x # see running commands
 DIR="${BASH_SOURCE%/*}"
 
+HOST_IP="$(ip route get 8.8.8.8 | head -1 | awk '{ print $7 }')"
+
+grep localhost /etc/hosts > /tmp/hosts
+grep -v localhost /etc/hosts | sed -r "s/127\.[0-9]+\.[0-9]+\.[0-9]+/${HOST_IP}/g" >> /tmp/hosts
+echo "${HOST_IP} host.local" >> /tmp/hosts
+
 docker run -ti --rm \
-  -e DISPLAY=$DISPLAY \
+  -e DISPLAY="${DISPLAY}" \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v "$(pwd)":"/mnt/nosetests" \
+  -v /tmp/hosts:/etc/hosts \
+  -v "$(pwd)":/mnt/nosetests \
   -u "$(id -u "${USER}")":"$(id -g "${USER}")" \
   petrknap/selenium-needle bash -c "cd /mnt/nosetests && nosetests $*" \
 ;
