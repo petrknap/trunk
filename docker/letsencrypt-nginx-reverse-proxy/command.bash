e7032a563733497384a241a5dddd33dceb55d042
Petr Knap <dev@petrknap.cz>
2022-04-22 15:48:14 +0200

    feat: health check moved into Dockerfile

--- docker/letsencrypt-nginx-reverse-proxy/command.bash
+++ docker/letsencrypt-nginx-reverse-proxy/command.bash
@@ -3,14 +3,9 @@ set -e
 
 bash /prepare_nginx.bash
 nginx -g "daemon off;" &
-bash /call_letsencrypt.bash
-nginx -s reload
 
 while (true); do (
-    for I in $(seq 1 100800); do (
-        sleep 5
-        curl --max-time 2 --fail http://localhost/ > /dev/null 2>&1
-    ); done
     bash /call_letsencrypt.bash
     nginx -s reload
+    sleep 1d
 ); done
